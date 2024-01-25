d101f923f4d94d0f82dd8ade040976e53de81c9b
Itsspacenug <149199513+Itsspacenug@users.noreply.github.com>
2024-01-25 12:53:45 -0600

    add more files

---
+++ src/BSTReplicated.java
@@ -0,0 +1,26 @@
+/*
+ * BSTReplicated.java
+ * 
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class BSTReplicated  {
+ 
+    public static void main(String s[]) 
+    {
+    	
+		//Students create 2 BinarySearchInt objects.
+    		// a) Uses the default constructor
+    		// b) Uses the the construtor that allows an integer value to be passed in.
+    	
+		
+      	//Test out each of your various methods (getSize, clear, isEmpty, add, findParent, contains, remove, toString)) 
+      		//using either of the instantiated BinarySearchIntTree objects
+      		
+    }
+    
+	//Create your BinarySearchIntTree class in another file called "BinarySearchIntTree"
+}
